43330e0e6ad8a193703e37e5b7402db158d0aaa6
chrissergio96 <safou.christiansergio@gmail.com>
2025-04-08 12:19:22 +0100

    'map'

--- src/CartePortGentil/CartePortGentil.js
+++ src/CartePortGentil/CartePortGentil.js
@@ -7,26 +7,67 @@ const dataArrondissements = [
   {
     name: "1er Arrondissement",
     coords: [
-      [-0.709, 8.784], [-0.712, 8.79], [-0.718, 8.785], [-0.715, 8.777]
+      [-0.6874841, 8.7667810], [-0.7142010, 8.7862120], [-0.7185334, 8.7774593], [-0.7163216, 8.7694520], [-0.7184652, 8.7555585], [-0.7024199, 8.7613095]
     ],
     winner: "Alain Claude Billie Bi Nze",
     centres: [
-      { nom: "Centre de vote A1", adresse: "École Martine Orou Gabon" },
-      { nom: "Centre de vote A2", adresse: "Lycée Evangélique" }
+      { nom: "Centre de vote 1", adresse: "École Martine Publique Balise 2 - NGADI" },
+      { nom: "Centre de vote 2", adresse: "École Joseph AMBOUROUET AVARO" },
+      { nom: "Centre de vote 3", adresse: "Centre de Formation Professional et de Perfectionnement" },
+      { nom: "Centre de vote 4", adresse: "École publique de la Cité" }
+
     ],
     color: "blue"
   },
   {
     name: "2e Arrondissement",
     coords: [
-      [-0.720, 8.790], [-0.725, 8.795], [-0.730, 8.788], [-0.726, 8.780]
+      [-0.7151979, 8.7862327], [-0.7191433, 8.7778459], [-0.7173559, 8.7689663], [-0.7196959, 8.7560876], [-0.7277240, 8.7525822], [-0.7319684, 8.7495588], [-0.7421631, 8.7596250], [-0.7455053, 8.7679787], [-0.7455053, 8.7679787], [-0.7336982, 8.7742638], [-0.7258702, 8.7800666], [-0.7258702, 8.7800666], [-0.7258702, 8.7800666], [-0.7213049, 8.7885773]
+    ],
+  
+    color: "green" 
+  },
+  {
+    name: "Mairie du 2e Arrondissement",
+    centre:"principal",
+    coords: [
+      [-0.7265175, 8.7742088],
+       [-0.7264208, 8.7737821], 
+       [-0.7268167, 8.7737123], 
+       [-0.7268737, 8.7741605]
     ],
     winner: "Candidat B",
     centres: [
-      { nom: "Centre de vote B1", adresse: "École Michel Dirat" },
-      { nom: "Centre de vote B2", adresse: "CEM du 2e Arrondissement" }
+      { nom: "Centre de vote 1", adresse: "Centre Social" },
+      { nom: "Centre de vote 2", adresse: "École publique Abbé YOYA" },
+      { nom: "Centre de vote 3", adresse: "École publique AMBOUROUE AVARO" },
+      { nom: "Centre de vote 4", adresse: "École Sainte Thérèse" },
+      { nom: "Centre de vote 5", adresse: "École publique Ancienne Balise" },
+      { nom: "Centre de vote 6", adresse: "École Prostestante" },
+      { nom: "Centre de vote 7", adresse: "École du Stade Blanc 1 - Balise" },
+      { nom: "Centre de vote 8", adresse: "École du Stade Blanc 2 - Henri Clément" },
+
     ],
-    color: "green"
+    color: "white" 
+  },
+  {
+    name: "Mairie du 1er Arrondissement",
+    centre:"principal",
+
+    coords: [
+      [-0.7162321, 8.7759428], 
+      [-0.7178093, 8.7749528],
+      [-0.7180088, 8.7767323]
+    ],
+    winner: "Alain Claude Billie Bi Nze",
+    centres: [
+      { nom: "Centre de vote 1", adresse: "École Martine Publique Balise 2 - NGADI" },
+      { nom: "Centre de vote 2", adresse: "École Joseph AMBOUROUET AVARO" },
+      { nom: "Centre de vote 3", adresse: "Centre de Formation Professional et de Perfectionnement" },
+      { nom: "Centre de vote 4", adresse: "École publique de la Cité" }
+
+    ],
+    color: "red" 
   }
 ];
 
@@ -34,12 +75,14 @@ const CartePortGentil = () => {
   const [selectedArrondissement, setSelectedArrondissement] = useState(null);
 
   const handleClick = (arrondissement) => {
-    setSelectedArrondissement(arrondissement);
+    if (arrondissement.centre === "principal") {
+      setSelectedArrondissement(arrondissement);
+    }
   };
+  
 
   return (
     <div className="map-wrapper">
-      <h2>Carte Interactive - Port-Gentil</h2>
       <MapContainer center={[-0.720, 8.78]} zoom={14} scrollWheelZoom={false} className="map">
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
