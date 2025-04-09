e524e9aacfcc80a940054fc820f1e98a5111ca07
chrissergio96 <safou.christiansergio@gmail.com>
2025-04-09 17:57:51 +0100

    'anime'

--- src/PageAccueil/PageAccueil.js
+++ src/PageAccueil/PageAccueil.js
@@ -7,26 +7,40 @@ import blason from '../Images/logoministereinterieur.jpg';
 const PageAccueil = () => {
   return (
     <div className='presidentielle'>
-      <div className="header">
-        <img src={logoMinistere} alt="Logo Ministère de l'intérieur" className="logo-gauche" />
-        <div className='mereelec'>
-        <h1 className="titre">
-          <span className="elections">ÉLECTION<img src="https://d24irw6hr5upwc.cloudfront.net/641-large_default/drapeau-gabon-5075-cm.jpg" alt="carte" className="logo-gabon" />    <p>12 AVRIL</p>        </span>{' '}
-          </h1>
-          <h1 className='titrepresi'>
-          <span className="presidentielle-text">PRÉSIDENTIELLE <h4>2025</h4></span>{' '}
-          </h1>
-        </div>
-       
+  <div className="header">
+    <img
+      src={logoMinistere}
+      alt="Logo Ministère de l'intérieur"
+      className="logo-gauche logo-animation"
+    />
 
-        <img src={blason} alt="Blason officiel" className="logo-droit" />
-      </div>
-      <div className='rectangle'>
-        <h6>COMMISSION ELECTORALE LOCALE</h6>
-        <h6>1er - 2ème ARRONDISSEMENT</h6>
+    <div className='mereelec text-fade-in'>
+      <h1 className="titre">
+        <span className="elections">
+          ÉLECTION
+          <img src="https://d24irw6hr5upwc.cloudfront.net/641-large_default/drapeau-gabon-5075-cm.jpg" alt="carte" className="logo-gabon" />
+          <p>12 AVRIL</p>
+        </span>
+      </h1>
 
-      </div>
+      <h1 className='titrepresi'>
+        <span className="presidentielle-text">PRÉSIDENTIELLE <h4>2025</h4></span>
+      </h1>
     </div>
+
+    <img
+      src={blason}
+      alt="Blason officiel"
+      className="logo-droit logo-animation"
+    />
+  </div>
+  
+  <div className='rectangle text-fade-in'>
+    <h6>COMMISSION ELECTORALE LOCALE</h6>
+    <h6>1er - 2ème ARRONDISSEMENT</h6>
+  </div>
+</div>
+
   );
 };
 
